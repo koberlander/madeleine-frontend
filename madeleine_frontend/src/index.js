c0c2a2602a0d18cb89d516884a83e21b2c01110c
github username <github email address>
2019-05-08 12:25:02 -0400

    load articles via getArticles

--- madeleine_frontend/src/index.js
+++ madeleine_frontend/src/index.js
@@ -1,8 +1,15 @@
-document.addEventListener('DOMContentLoaded', {
+document.addEventListener('DOMContentLoaded', () => {
+// load DOM elements
 let articleSearch = document.getElementById('article-search')
+
 })
 
+// add Event Listeners
+
+
+// helper functions
 function getArtcles(){
-  fetch('')
-  .get()
+  fetch('http://localhost:3000/api/v1/articles')
+  .then(res => res.json())
+  .then(json => console.log(json))
 }
