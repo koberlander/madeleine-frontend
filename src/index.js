a756689c47997b58dac720bc417cc3a96d974c48
github username <github email address>
2019-05-10 09:43:24 -0400

    css and bootstrap, secure api key

--- src/index.js
+++ src/index.js
@@ -41,56 +41,73 @@ function getArticles(){
   function displaySearchResults(data){
     data["response"]["docs"].forEach(article => {
       chewJSON(article)
+
+      // find container-fluid, add id
+      let searchCard = document.getElementById('styled-search-div')
+
+      //find article results div
+      let searchResults = document.getElementById('search-div')
+
+      // create card divs <div class="card w-75"> and <div class="card-body">
+      let cardDivParent = document.createElement('div')
+        cardDivParent.className = 'card w-75'
+        cardDivParent.setAttribute('id', 'card-w-75-id')
+
+      let cardDivChild = document.createElement('div')
+        cardDivChild.className = 'card-body'
+        cardDivChild.setAttribute('id', 'card-body-id')
+
+      // make headline <h5>, author <p>, and snippet <p> divs
+      let headline = document.createElement('h5')
+        headline.className = 'card-title'
+        headline.setAttribute('id', 'card-title-id')
+        headline = article.headLine
+
+      let author = document.createElement('p')
+        author.className = 'card-text'
+        author.setAttribute('id', 'card-text-id')
+        author = article.author
+
+      let snippet = document.createElement('p')
+        snippet.className = 'card-text'
+        snippet.setAttribute('id', 'card-text-id-2')
+        snippet = article.snippet
+
+      //append article content tags to card div
+      cardDivChild.append(headline, author, snippet)
+
+
+      //append card div to parent
+      cardDivParent.append(cardDivChild)
+
+      //append parent to article results div
+      searchResults.append(cardDivParent)
+
+
+      // append searchResults to container-fluid
+      searchCard.append(searchResults)
+
+
+      // return cardDivParent
+      return searchCard
+
     })
-
-    //       <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    //         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    //   </div>
-    // </div>
-
-    //find article results div
-    // let searchResults = document.getElementById('search-results')
-    //
-    //
-    // // create card divs <div class="card w-75"> and <div class="card-body">
-    // let cardDivParent = document.createElement(div)
-    // cardDivParent.classname('card w-75')
-    // cardDivParent.setAttribute('id', 'card-w-75-id')
-    //
-    // let cardDivChild = document.createElement(div)
-    // cardDivChild.classname('card-body')
-    // cardDivChild.setAttribute('id', 'card-body-id')
-    //
-    // //make headline <h5>, author <p>, and snippet <p> divs
-    //
-    // let headline = document.createElement(h5)
-    // headline.classname('card-title')
-    // headline.setAttribute('id', 'card-title-id')
-    // headline.innerText = ??????
-
-    //append article content tags to card div
-
-    //append card div to parent
-
-    //append parent to article results div
-
-    // return cardDivParent
-
   }
+
 
   function chewJSON(article) {
     allArticles = []
     // this will take in the massive NYT response JSON and spit out the
     // clean / simplified data we'll be displaying/persisting
-    headLine = article["headline"]["main"]
-    author = article["byline"]["original"]
-    pubDate = article["pub_date"]
-    snippet = article["snippet"]
-    webUrl = article["web_url"]
-    keywords = []
-    aKeyword = article["keywords"]
-    articleObject = {"headLine": headLine, "author": author, "pubDate": pubDate, "snippet": snippet, "webUrl": webUrl}
-    debugger
+
+    let headLine = article["headline"]["main"]
+    let author = article["byline"]["original"]
+    let pubDate = article["pub_date"]
+    let snippet = article["snippet"]
+    let webUrl = article["web_url"]
+    let keywords = []
+    let aKeyword = article["keywords"]
+    let articleObject = {"headLine": headLine, "author": author, "pubDate": pubDate, "snippet": snippet, "webUrl": webUrl}
     // article = Article.find_or_create_by(article_object)
-    allArticles << article
+    return allArticles.push(articleObject)
   }
